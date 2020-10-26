94e03a860a6059ecb2b16d77223bc88afb22b5bb
sasilva1998 <sasilva1998@gmail.com>
2020-10-26 17:05:55 -0500

    motors control

--- uPy/motor_control.py
+++ uPy/motor_control.py
@@ -1,6 +1,7 @@
 import uros
 from std_msgs import Int16  # rosserial messages
-from machine import Pin
+from machine import Pin, PWM
+import machine
 import utime
 import gc
 gc.enable()
@@ -12,35 +13,30 @@ class MotorControl(
 
         # constants
         self.error_topic = "/motors_pwr"
-	self.vel = 10
+	self.node = uros.NodeHandle(2, 115200, tx=17, rx=16)
 	
 	#motor1
-	self.ENA = Pin(13,Pin.OUT)
+	self.ENA = PWM(Pin(2), freq=20000, duty=620)
 	self.IN1 = Pin(12,Pin.OUT)
 	self.IN2 = Pin(14,Pin.OUT)
-	self.ena_pwm=machine.PWM(self.ENA)
-
-    	self.ena_pwm.freq(1000)
-    	self.ena_pwm.duty(self.vel)
 
 	#motor2
-	self.ENB = Pin(25,Pin.OUT)
+	self.ENB = PWM(Pin(0), freq=20000, duty=620)
 	self.IN3 = Pin(27,Pin.OUT)
 	self.IN4 = Pin(26,Pin.OUT)
-	self.enb_pwm=machine.PWM(self.ENB)
-	self.enb_pwm.freq(1000)
-    	self.enb_pwm.duty(self.vel)
+
+	self.stop()
 
     def turn_right(self):
+	self.IN1.value(0)
+	self.IN2.value(0)
+	self.IN3.value(0)
+	self.IN4.value(1)
+
+    def turn_left(self):
 	self.IN1.value(1)
 	self.IN2.value(0)
 	self.IN3.value(0)
-	self.IN4.value(0)
-
-    def turn_left(self):
-	self.IN1.value(0)
-	self.IN2.value(0)
-	self.IN3.value(1)
 	self.IN4.value(0)
 
     def stop(self):
@@ -52,14 +48,15 @@ class MotorControl(
     def go_straight(self):
 	self.IN1.value(1)
 	self.IN2.value(0)
-	self.IN3.value(1)
-	self.IN4.value(0)
+	self.IN3.value(0)
+	self.IN4.value(1)
 	
     def error_callback(self, msg):
         pos_err =  msg.data
-        if pos_err<-5:
+	print(pos_err)
+        if pos_err<-0:
 		self.turn_left()
-	if pos_err>5:
+	if pos_err>0:
 		self.turn_right()
 	else:
 		self.go_straight()
@@ -73,5 +70,12 @@ class MotorControl(
 def start_robot():
 	robot = MotorControl()
 	robot.start()
+def test():
+	robot = MotorControl()
+	robot.go_straight()
+	utime.sleep(4)
+	robot.stop()
+
+	
 
 
